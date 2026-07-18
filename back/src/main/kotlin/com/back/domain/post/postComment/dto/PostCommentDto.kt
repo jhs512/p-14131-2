3461c651e280324eedaa1e16b70c6eec5276d9ee
jhs512 <jangka512@gmail.com>
2026-07-18 17:16:06 +0900

    002

---
+++ back/src/main/kotlin/com/back/domain/post/postComment/dto/PostCommentDto.kt
@@ -0,0 +1,24 @@
+package com.back.domain.post.postComment.dto
+
+import com.back.domain.post.postComment.entity.PostComment
+import java.time.LocalDateTime
+
+data class PostCommentDto(
+    val id: Int,
+    val createDate: LocalDateTime,
+    val modifyDate: LocalDateTime,
+    val authorId: Int,
+    val authorName: String,
+    val postId: Int,
+    val content: String
+) {
+    constructor(postComment: PostComment) : this(
+        postComment.id,
+        postComment.createDate,
+        postComment.modifyDate,
+        postComment.author.id,
+        postComment.author.name,
+        postComment.post.id,
+        postComment.content
+    )
+}
